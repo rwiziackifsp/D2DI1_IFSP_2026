b812dc40c9b3d99d1ce077b8d4e8cd115f07b825
rwiziackifsp <r.wiziack@aluno.ifsp.edu.br>
2026-03-17 21:51:56 -0300

    Aula 17-03-2026
    CRUD completo

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('',views.main,name='main'),
     path('members/',views.members,name='members'),
     path('members/details/<int:id>',views.details,name='details'),
+    path('members/add/', views.add, name='add'),
+    path('members/add/addrecord/', views.addrecord, name='addrecord'),
+    path('members/delete/<int:id>', views.delete, name='delete'),
+    path('members/update/<int:id>', views.update, name='update'),
+    path('members/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
 ]
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Member
 
 
@@ -23,3 +24,33 @@ def details(request, id):
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    member = Member(first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def delete(request, id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('members'))
+
+def update(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    member = Member.objects.get(id=id)
+    member.first_name = request.POST['first_name']
+    member.last_name = request.POST['last_name']
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
