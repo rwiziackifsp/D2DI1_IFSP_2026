7176f510413c60ca4ca7deba6ba2f05c17378a7f
rwiziackifsp <r.wiziack@aluno.ifsp.edu.br>
2026-05-05 21:35:14 -0300

    Aula 8 - my_tennis_club_aula6-main
    Inserção e atualização de:
    Foto
    Documento

---
+++ members/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from plans.models import Plan
+
+class Member(models.Model):
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, default=1)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+    foto = models.ImageField(upload_to='images/', null=True)
+    documento = models.FileField(null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
---
+++ members/views.py
@@ -0,0 +1,101 @@
+from multiprocessing import context
+
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from members.models import Member
+from plans.models import Plan
+
+
+def members(request):
+    mymembers = Member.objects.all()
+    template = loader.get_template("all_members.html")
+
+    paginator = Paginator(mymembers, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {
+        "mymember": mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
+
+def add(request):
+    myplans = Plan.objects.all().values()
+    template = loader.get_template("add.html")
+    context = {
+        'myplans': myplans
+    }
+    return HttpResponse(template.render(context, request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    z = Plan.objects.filter(id=request.POST['plan']).first()
+    foto = None
+    if 'foto' in request.FILES:
+        foto = request.FILES['foto']
+    if 'documento' in request.FILES:
+        documento = request.FILES['documento']
+    member = Member(firstname=x, lastname=y, plan=z, foto=foto, documento=documento)
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def delete(request, id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('members'))
+
+def update(request, id):
+    mymember = Member.objects.get(id=id)
+    myplans = Plan.objects.all().values()
+    template = loader.get_template('update.html')
+    context = {
+        "mymember": mymember,
+        "myplans": myplans,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    plan = request.POST['plan']
+    member = Member.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.plan = Plan.objects.get(id=plan)
+    if 'foto' in request.FILES:
+        foto = request.FILES['foto']
+        member.foto = foto
+    if 'documento' in request.FILES:
+        documento = request.FILES['documento']
+        member.documento = documento
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def busca(request):
+    texto_busca = request.POST['busca']
+    dados = Member.objects.filter(
+        Q(firstname__contains=texto_busca) | Q(lastname__contains=texto_busca)
+    ).values()
+    t = loader.get_template('all_members.html')
+    context = {
+        'mymembers': dados,
+        'texto_busca': texto_busca,
+        'qtd': len(dados),
+    }
+    return HttpResponse(t.render(context, request))
